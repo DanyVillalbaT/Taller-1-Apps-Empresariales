17d8d97014c4f3f08c501c80f31e8f43586c70ed
DanyVillalbaT <villalba.daniela.0874@eam.edu.co>
2023-08-30 11:26:20 -0500

    Creada la interfaz de Pais

---
+++ src/main/java/com/daniela/repository/IPaisRepository.java
@@ -0,0 +1,9 @@
+package com.daniela.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.daniela.models.Pais;
+
+public interface IPaisRepository extends JpaRepository<Pais, Integer>{
+
+}
